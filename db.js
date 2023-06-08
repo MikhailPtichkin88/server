054bcc1b10e338a7948b4d60908189355ef9edd1
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2023-06-08 11:48:00 +0300

    fix bugs

--- db.js
+++ db.js
@@ -3,6 +3,12 @@ const { pg } = require('pg');
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'postgres',
   dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
 });
